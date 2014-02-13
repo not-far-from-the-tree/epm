268d220b32ad5f0ca285f9a83d306d0e17843658
Michael Fagan <mfagan@gmail.com>
2014-02-13 16:35:51 -0500

    default rails app without test unit and gems for jbuilder and coffeescript

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Epm::Application.config.secret_key_base = 'ab09188f6de78b04dcc2f8f13c11a31b4d9ff738d3d456f31058b25d8f700d208ecd9fc1a35f87204b4cdddbea2c1e6eb52c2fc3a6385a50d375f846a87afe58'
