46499a72627ae7b1e7820f7a0b908e2adaf78a02
Michael Fagan <mfagan@gmail.com>
2014-02-16 22:05:10 -0500

    fix for association between events and users

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,6 @@ class Event < ActiveRecord::Base
   validates :start, :finish, presence: true
 
   has_many :event_users
-  has_many :participants, through: :event_users, class_name: :user
+  has_many :participants, through: :event_users, source: :user
 
 end
