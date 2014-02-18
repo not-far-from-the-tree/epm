4862da2dd68996a3e4d6895113e17a8dd1e0f0bf
Michael Fagan <mfagan@gmail.com>
2014-02-18 15:16:12 -0500

    changing event factory and thus demo data to have events only at integer hours, no minutes

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,12 +1,12 @@
 FactoryGirl.define do
 
   factory :event do
-    start { rand(100).days.from_now + (rand(12)+7).hours }
+    start { rand(100).days.from_now.change hour: rand(12)+7 }
     finish { (start || Time.now) + (rand(4)+1).hours }
   end
 
   factory :past_event, class: Event do
-    start { (rand(100)+1).days.ago + (rand(12)+7).hours }
+    start { (rand(100)+1).days.ago.change hour: rand(12)+7 }
     finish { start + (rand(4)+1).hours }
   end
 
