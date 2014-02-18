a1c179bdbac748e6a64cccdefe2f19c706868313
Michael Fagan <mfagan@gmail.com>
2014-02-17 23:56:20 -0500

    small fix for event factory in prior comment. was causing test that created an invalid event to fail

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :event do
     start { rand(100).days.from_now + (rand(12)+7).hours }
-    finish { start + (rand(4)+1).hours }
+    finish { (start || Time.now) + (rand(4)+1).hours }
   end
 
   factory :past_event, class: Event do
