6b5103f9aa640c971fbbc3a49d60445ef13a8d11
Michael Fagan <mfagan@gmail.com>
2014-03-05 19:08:47 -0500

    tiny fix to event factory just as in user factory in previous commit

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -10,7 +10,7 @@ FactoryGirl.define do
 
     factory :full_event do
       name { Faker::Lorem.words(rand 2..5).join(' ').capitalize }
-      description { Faker::Lorem.sentences(rand 1..5).join('. ') }
+      description { Faker::Lorem.sentences(rand 1..5).join(' ') }
     end
 
   end
@@ -21,7 +21,7 @@ FactoryGirl.define do
 
     factory :full_past_event do
       name { Faker::Lorem.words(rand 2..5).join(' ').capitalize }
-      description { Faker::Lorem.sentences(rand 1..5).join('. ') }
+      description { Faker::Lorem.sentences(rand 1..5).join(' ') }
     end
 
     factory :participatable_past_event do
