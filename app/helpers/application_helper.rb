cae1de421a343bdc2050e56d019e7784ccac20e7
Michael Fagan <mfagan@gmail.com>
2014-03-06 11:43:12 -0500

    ui/css improvements. adding two-col ui and using it for event form. fixing header for mobile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
     content_tag 'div', nil, class: 'clearfix'
   end
 
+  def start_cols
+    '<div class="cols"><div class="colA">'.html_safe
+  end
+  def next_col
+    '</div><div class="colB">'.html_safe
+  end
+  def end_cols
+    "</div></div>#{clear}".html_safe
+  end
+
 end
