4164fb73504b6b29214498e6b08b23c8d2fe9d61
Michael Fagan <mfagan@gmail.com>
2014-03-11 17:48:14 -0400

    now sending users to edit their profile after signing up. also new css for hints on form fields and using this to show which of your profile info will be public. minor test refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Epm::Application.routes.draw do
 
   root 'events#index'
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, only: [:index, :show, :edit, :update] do
     patch 'add_role', on: :member
   end
--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -2,63 +2,26 @@ require 'spec_helper'
 
 describe "Authentication" do
 
-  describe "signs up a new user" do
-    # consider merging some of these to improve speed as each hits the DB
-
-    def sign_up
-      visit new_user_registration_path
-      fill_in 'Email', :with => Faker::Internet.email
-      pass = Faker::Internet.password
-      fill_in 'Password', :with => pass
-      fill_in 'Password confirmation', :with => pass
-      click_button 'Sign up'
-    end
-
-    it "creates a new user" do
-      expect{ sign_up }.to change{User.count}.by 1
-    end
-
-    it "sends a confirmation email" do
-      expect{ sign_up }.to change{ActionMailer::Base.deliveries.size}.by 1
-      expect(last_email.to).to eq [User.last.email]
-      expect(last_email.from).to eq ['no-reply@example.com']
-    end
-
-    it "logs them in" do
-      sign_up
-      expect(page).to have_content 'signed up successfully'
-      expect(page).to have_content 'Log out'
-    end
-
-    it "returns them to the page they started from" do
-      visit root_path
-      sign_up
-      expect(current_path).to eq root_path
-    end
-
+  it "signs up a new user" do
+    visit new_user_registration_path
+    fill_in 'Email', :with => Faker::Internet.email
+    pass = Faker::Internet.password
+    fill_in 'Password', :with => pass
+    fill_in 'Password confirmation', :with => pass
+    expect{ click_button 'Sign up' }.to change{ActionMailer::Base.deliveries.size}.by 1
+    user = User.last
+    expect(last_email.to).to eq [user.email]
+    expect(last_email.from).to eq ['no-reply@example.com']
+    expect(current_path).to eq edit_user_path user
+    expect(page).to have_content 'signed up successfully'
+    expect(page).to have_content 'Log out'
   end
 
-  describe "fails to sign up an invalid user" do
-
-    def bad_sign_up
-      visit new_user_registration_path
-      click_button 'Sign up'
-    end
-
-    it "does not create a new user" do
-      expect{ bad_sign_up }.not_to change{User.count}
-    end
-
-    it "returns them to the sign up page" do
-      bad_sign_up
-      expect(current_path).to eq user_registration_path
-    end
-
-    it "displays an error message" do
-      bad_sign_up
-      expect(page).to have_content 'error'
-    end
-
+  it "fails to sign up an invalid user" do
+    visit new_user_registration_path
+    click_button 'Sign up'
+    expect(current_path).to eq user_registration_path
+    expect(page).to have_content 'error'
   end
 
   it "logs in a user" do
