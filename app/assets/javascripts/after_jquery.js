4b4bc7a3e85c60490a0c66d2fbac4687969db45c
Michael Fagan <mfagan@gmail.com>
2014-03-19 13:05:41 -0400

    adjusting input fields based on the content length on page load and when typing via js. borrowing some of this code from my other project

--- app/assets/javascripts/after_jquery.js
+++ app/assets/javascripts/after_jquery.js
@@ -1,1 +1,10 @@
 $('html').removeClass('no-js').addClass('js');
+
+$(function(){
+
+  // expand/contract input as content changes length
+  $("form[method!='get']").find("input[data-default_size]").keyup(function(){
+    $(this).attr('size', Math.min(100, Math.max($(this).data('default_size'), $(this).val().length)));
+  });
+
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,4 +23,37 @@ module ApplicationHelper
     "</div></div>#{clear}".html_safe
   end
 
+  # overwrite form helpers to have inputs auto-size to their content
+  def form_for(name, *args, &block)
+    options = args.extract_options!
+    # substitute our own form builder for the default one
+    super(name, *(args << options.merge(:builder => StandardFormBuilder)), &block)
+  end
+  def text_field_tag(name, value = nil, options = {})
+    options[:size] = 30 unless options.has_key? :size
+    options[:size] = [[options[:size], value.length].max, 100].min unless value.nil?
+    options['data-default_size'] = 30 unless options.has_key? :maxlength
+    super(name, value, options)
+  end
+
+end
+
+
+class StandardFormBuilder < ActionView::Helpers::FormBuilder
+
+  def self.create_tagged_field(method_name, default_size = 30)
+    define_method(method_name) do |label, *args|
+      args[0] = {} unless args.any?
+      # smart length expands from default (up to a max) based on the content
+      args[0][:size] = default_size unless args[0].has_key? :size
+      args[0][:size] = [[args[0][:size], @object[label].length].max, 100].min unless @object.new_record? || @object[label].nil?
+      args[0]['data-default_size'] = default_size unless args[0].has_key? :maxlength
+      super(label, *args)
+    end
+  end
+  create_tagged_field('text_field')
+  create_tagged_field('email_field')
+  create_tagged_field('telephone_field')
+  create_tagged_field('url_field', 52)
+
 end
