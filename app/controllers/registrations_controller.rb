519a505801e931cf0e5f0efdfc22a277bde58e8b
Michael Fagan <mfagan@gmail.com>
2014-05-27 12:10:33 -0400

    adding ui on user signup to add the user to the nfftt mailing list. no test for this yet

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,19 @@
+# https://github.com/plataformatec/devise/blob/master/app/controllers/devise/registrations_controller.rb
 class RegistrationsController < Devise::RegistrationsController
 
   before_action :configure_permitted_parameters
+
+  after_action :add_to_mailing_list, only: :create, if: "user_signed_in? && params[:add_to_mailing_list]"
 
   protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:sign_up) << :signed_waiver
+    end
+
+    def add_to_mailing_list
+      # todo: add feature test for this
+      current_user.add_to_mailing_list
     end
 
 end
