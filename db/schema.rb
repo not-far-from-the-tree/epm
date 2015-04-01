d368afb940080ed22177f6cc4b588b135c305226
bianca <bianca.sayan@gmail.com>
2015-03-31 23:45:14 -0400

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929213258) do
+ActiveRecord::Schema.define(version: 20150215181315) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,15 @@ ActiveRecord::Schema.define(version: 20140929213258) do
   end
 
   add_index "roles", ["user_id"], name: "index_roles_on_user_id", using: :btree
+
+  create_table "trees", force: true do |t|
+    t.string   "species"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.decimal  "lat",        precision: 9, scale: 6
+    t.decimal  "lng",        precision: 9, scale: 6
+  end
 
   create_table "user_wards", force: true do |t|
     t.integer  "user_id"
