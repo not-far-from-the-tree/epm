d513a64919410d1970d5b165be9457335c0fc009
bianca <bianca.sayan@gmail.com>
2015-05-05 21:31:00 -0400

    Allow admin to cancel on behalf of a participant

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -20,7 +20,7 @@ class Ability
 
       if user.has_role? :admin
         can :manage, [Event, Role, :setting]
-        cannot [:claim, :attend, :unattend], Event
+        cannot [:claim, :attend], Event
         can [:index, :map, :show, :read_contact, :read_attendance, :update, :destroy, :invite], User
       end
 
